86af816e257961132927e062b4cf6b2f8b29f4df
Miqueias Kawa <kawasousa1554@gmail.com>
2024-02-21 08:32:37 -0300

    name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ítalo Rodrigues - Ortopedia & Neurocirurgia Veterinária",
+  title: "Ítalo Rodriguis - Ortopedia & Neurocirurgia Veterinária",
   description: "Bem-vindo ao consultório do Dr. Ítalo Rodrigues, especializado em ortopedia e neurocirurgia veterinária. Oferecemos cuidados especializados e avançados para seus animais de estimação, garantindo o melhor tratamento para suas necessidades ortopédicas e neurológicas. Conte com nossa experiência e dedicação para cuidar da saúde e bem-estar do seu companheiro peludo.",
 };
 
